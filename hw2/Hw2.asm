7fa6f4a0559b97daa297e95590bdc9664bc7148c
mihairusu05 <mihai10rusu+github@gmail.com>
2025-11-06 00:00:00 +0200

    Added Hw2

---
+++ hw2/Hw2.asm
@@ -0,0 +1,46 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    a db 3
+    b db 3
+    d dw 0
+
+; our code starts here
+
+;3. [-1+d-2*(b+1)]/a data types: a,b,c - byte, d - word and ->>>> -3 result
+;second expression: (8-a*b*100+c)/d+x; a,b,d-byte; c-doubleword; x-qword
+
+segment code use32 class=code
+    start:
+        mov AL, [b]
+        add AL, 1
+        mov Bl, 2
+        imul Bl
+        
+        ;2*(b+1)->>
+        
+        mov BX, AX
+        
+        mov AX, [d]
+        sub AX, 1
+        
+        ;-1 + d
+        
+        sub AX, BX
+        mov CL, [a]
+        idiv CL
+        
+        ;(-1 + d - 2*(b + 1))/a
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
---
+++ hw2/hw2ex2.asm
@@ -0,0 +1,58 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; ...
+    a db 0
+    b db 10
+    d db 2
+    c dd 4
+    x dq 10
+    
+    ;result should be 16
+
+; our code starts here
+segment code use32 class=code
+;(8-a*b*100+c)/d+x
+    start:
+        mov AL, [d]
+        cbw
+        cwde
+        mov EBX, EAX
+        
+        mov AL, [a]
+        mov CL, [b]
+        imul CL
+        mov CX, 100
+        imul CX
+        
+        push DX
+        push AX
+        pop EAX
+        
+        mov ECX, 8
+        sub ECX, EAX
+        add ECX, [c]
+        mov EAX, ECX
+        
+        cdq
+        idiv EBX
+        cdq
+        
+        mov EBX, [x + 0]
+        mov ECX, [x + 4]
+        
+        add EAX, EBX
+        add EDX, ECX
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
