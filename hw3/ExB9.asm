948f6f2801836bd8cdeb05d170e7c91d3a3a8e81
mihairusu05 <mihai10rusu+github@gmail.com>
2025-11-19 16:40:53 +0200

    Small changes

--- hw3/ExB9.asm
+++ hw3/ExB9.asm
@@ -5,24 +5,19 @@ import exit msvcrt.dll
 
 segment data use32 class=data
     S dd 12345678h, 1a2b3c4dh
-    
     len equ ($ - S) / 4
-    
     D times len db 0
 
 segment code use32 class=code
 start:
     mov ecx, len       
     jecxz end_program  
-
     mov esi, 0         
     mov edi, 0         
 
 my_loop:
     mov eax, [S + esi] 
-                       
     mov [D + edi], al  
-
     add esi, 4         
     inc edi            
 
